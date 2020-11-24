d8722e6430f919a84d6d9b5b9f0b508557660f40
Philip Verwegen <pvrw@gmx.de>
2020-11-24 18:34:15 +0100

    Some more changes

--- test_python_file.py
+++ test_python_file.py
@@ -1,5 +1,10 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 a = np.array([1,2,3,4,5])
+b = np.array([3,4,5,8,7])
 print(a)
 print("Here we did some changes as well")
+
+plt.figure()
+plt.plot(a,b)
+plt.show()
